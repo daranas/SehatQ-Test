1116b7ffed48964ee681361d8b57c85d57ba1f8b
Darana Vidya <darana.sv@gmail.com>
2020-05-30 00:20:16 +0700

    change margin

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -5,7 +5,7 @@ import { createUseStyles } from 'react-jss';
 const useStyles = createUseStyles({
   topbarWrapper: {
     paddingTop: '12px!important',
-    marginBottom: '5px!important'
+    marginBottom: '10px!important'
   },
   iconLeft: {
     fontSize: 26,
